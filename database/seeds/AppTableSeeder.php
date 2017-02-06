a9839cbe8a2c4fc3ffc9bfc61612e84eeda91204
EstebanKuh <Esteban.kuh@gmail.com>
2017-02-06 13:47:39 -0600

    Agregando seeds

---
+++ database/seeds/AppTableSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Seller;
+use App\Product;
+use App\Review;
+use App\Tag;
+
+class AppTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $numOfTags = 5;
+        $numOfSellers = 2;
+        $numOfProducts = 3;
+        $numOfSellerAddresses = 2;
+        $numOfReviews = 60;
+
+        $tags = factory(Tag::class,$numOfTags)->create();
+        $sellers = factory(Seller::class,$numOfSellers)->create();
+
+        foreach($sellers as $seller){
+            $products= factory(Product::class, $numOfProducts)
+                -> create(['seller_id' => $seller->id])
+                    -> each (
+                        function ($product){
+                            $product ->tags()->sync(
+                                Tag::all()->random(2)
+                            );
+                        }
+                    );
+        }
+
+        foreach($products as $product){
+            factory(Review::class, $numOfReviews)->create(['product_id' => $product->id]);
+        }
+
+
+    }
+}
